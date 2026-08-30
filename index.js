28b278e2078a2b1a35b399c55be0506b2f56decb
Adrian Moldovan <ma.moldovan@gmail.com>
2026-08-30 13:48:48 +0300

    adds random number and letter implementation

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Starting custom random number generator...');
+import express from 'express';
+
+const app = express();
+const _PORT = 3000;
+
+console.log('Starting random generator...');
+
+app.get('/', (req, res) => {
+    res.json({
+        appName: "Random generator",
+        status: "Up and running!",
+        availableEndpoints: [
+            {
+                url: "/number",
+                description: "generate a random number"
+            },
+            {
+                url: "/number",
+                description: "generate a random letter"
+            }
+        ]
+    });
+});
+
+
+app.get('/number', (req, res) => {
+    res.json({
+        description: "Generates a random number",
+        value: Math.floor(Math.random() * 100)
+    });
+})
+
+
+app.get('/letter', (req, res) => {
+    res.json({
+        description: "Generates a random letter",
+        value: String.fromCharCode(65 + Math.floor(Math.random() * 26))
+    });
+})
+
+
+app.listen(3000, () => {
+    console.log(`Server running on http://localhost:${_PORT}`);
+});
